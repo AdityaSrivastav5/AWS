8dc75b7f573e7e9433dadeadaab553eea55b89c9
AdityaSrivastav5 <adityasrivastav863@gmail.com>
2025-02-13 14:12:31 +0530

    ProcessScheduler

---
+++ ProcessScheduler/models/Process.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const processSchema = new mongoose.Schema({
+  name: { type: String, required: true }, 
+  frequency: { type: String, enum: ["daily", "weekly"], required: true },
+  lastRun: { type: Date, default: null }, 
+  isActive: { type: Boolean, default: false }, 
+});
+
+module.exports = mongoose.model("Process", processSchema);
